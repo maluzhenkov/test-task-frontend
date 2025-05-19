f53481b85f0089825db92e71ae561911e45b2543
Nikolay Maluzhenkov <Kolyan57024@mail.ru>
2025-05-19 15:56:52 +0300

    completed task

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <MultipleSelector v-model="multipleSelected" :list="firstData" :max="6" />
+  <SingleSelector v-model="singleSelected" :list="secondData" />
+</template>
+
+<script setup lang="ts">
+import MultipleSelector from '@/components/MultipleSelector.vue'
+import SingleSelector from '@/components/SingleSelector.vue'
+import mockData from '@/mock/index.json'
+import type { IClothItem } from '@/types'
+import { ref } from 'vue'
+
+const { firstData, secondData } = mockData
+
+const multipleSelected = ref<IClothItem[]>([])
+const singleSelected = ref<IClothItem | null>(null)
+</script>
+
+<style scoped></style>
